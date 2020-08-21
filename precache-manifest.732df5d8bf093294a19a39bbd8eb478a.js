456ddae54944b11995e9e91bfbfdc40fa420afed
Gaurav Raj Sinha <gauravraj.sinha@singleinterface.com>
2020-08-21 08:10:37 +0530

    Updates

---
+++ precache-manifest.732df5d8bf093294a19a39bbd8eb478a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082b6834f09b520339cafe0594df5280",
+    "url": "./index.html"
+  },
+  {
+    "revision": "394b4f57d79c2c79b80b",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "6d47b27e50aef520bcdd",
+    "url": "./static/js/2.cbfc97b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.cbfc97b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "394b4f57d79c2c79b80b",
+    "url": "./static/js/main.c01c873e.chunk.js"
+  },
+  {
+    "revision": "f326d7df967ac8810b61",
+    "url": "./static/js/runtime-main.ad799923.js"
+  }
+]);
